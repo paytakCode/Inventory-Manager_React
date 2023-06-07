5277ece33d3bee40f15d564da3b21529ab815279
paytakcode <paytakcode@gmail.com>
2023-06-08 05:04:32 +0900

    Fix: PurchaseStatus 오타

--- src/components/Base/ProductionStatus.ts
+++ src/components/Base/ProductionStatus.ts
@@ -1,7 +1,8 @@
-enum PurchaseStatus {
+enum ProductionStatus {
     PLANNED = "생산예정",
-    PRODUCTION = "생산중",
-    COMPLETED = "완료",
+    INPRODUCTION = "생산중",
+    COMPLETED = "생산완료",
     CANCELED = "취소"
 }
-export default PurchaseStatus;
+
+export default ProductionStatus;
