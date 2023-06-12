6b9e062a7e43a7e7be2ff58e37ed94ded4faa4c2
paytakcode <paytakcode@gmail.com>
2023-06-13 00:03:02 +0900

    Feat: Default Message 추가 및 IntroPage 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import IntroPage from "./pages/IntroPage";
 import MainPage from './pages/MainPage';
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -11,7 +10,7 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path='/' Component={IntroPage}/>
+                <Route path='/' Component={MainPage}/>
                 <Route path='/main' Component={MainPage}/>
                 <Route path='/login' Component={LoginPage}/>
                 <Route path='/register' Component={RegisterPage}/>
--- src/pages/Content/DefaultContent.tsx
+++ src/pages/Content/DefaultContent.tsx
@@ -1,14 +1,32 @@
 import React from "react";
+import {getCurrentUserInfo} from "../../services/userService";
+import {isLogined} from "../../services/authService";
+import styles from "pages/Content/CommonContent.module.scss";
 
 const DefaultContent = () => {
+
     return (
-        <>
-            <div>DefaultContent</div>
-            <div>
-                Default Page
-            </div>
-        </>
+        <div className={styles.content}>
+            <div className={styles.title}>Inventory Manager Project</div>
+            <div><Message/></div>
+        </div>
     );
 };
 
+const Message: React.FC = () => {
+    const currentUserInfo = getCurrentUserInfo();
+
+    if (isLogined()) {
+        return (<div>
+            {currentUserInfo.name}님 안녕하세요. <br/>
+            상단 메뉴를 이용하여 서비스를 이용해주세요.
+        </div>);
+    } else {
+        return (<div>
+            서비스를 이용하시려면
+            우측 상단 버튼을 눌러 등록 및 로그인해주세요.
+        </div>);
+    }
+    ;
+};
 export default DefaultContent;
